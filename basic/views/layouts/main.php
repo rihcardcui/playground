a17f82b2327c0453f699845e6a331f83e8140398
Richard <richardcui@ideaslab.hk>
2018-06-08 12:20:54 +0800

    no message

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -40,6 +40,7 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
+	        ['label' => 'Country', 'url' => ['/country/index']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
